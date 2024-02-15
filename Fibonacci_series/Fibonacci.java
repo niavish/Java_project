9d6020ac3d77e74122d17b77639ccc37420a5bd3
Neha Vishan <56451637+niavish@users.noreply.github.com>
2024-02-15 05:12:58 +0000

    fibo

--- Fibonacci_series/Fibonacci.java
+++ Fibonacci_series/Fibonacci.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 class Fibonacci {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Fibonacci series  : 0,1,1,2,3,5,8,13,21,34,55,....
+        System.out.print("How many fiboannci numbers you want : ");
+        int num = sc.nextInt();
+        int fibo_pre = 0, fibo = 1, sum = 0;
+
+        for(int i=0; i<num; i++) {
+            System.out.println(fibo_pre);
+            sum = fibo + fibo_pre;
+            fibo_pre = fibo;
+            fibo = sum;
+        }
+    }
 }
